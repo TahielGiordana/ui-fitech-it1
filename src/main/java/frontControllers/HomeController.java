754a90eb29de03f2afd5b45334a992259eacfae8
TahielGiordana <tahielgiordana@gmail.com>
2023-04-17 22:23:47 -0300

    Inicializo el core en homeController

--- src/main/java/frontControllers/HomeController.java
+++ src/main/java/frontControllers/HomeController.java
@@ -1,5 +1,6 @@
 package frontControllers;
 
+import init.InitCore;
 import pojos.Machine;
 import pojos.Person;
 import services.RoutineFinder;
@@ -7,20 +8,14 @@ import validators.RoutineValidator;
 
 public class HomeController {
 
-    /*public static void main(String[] args) {
-        Person person = new Person(new Routine("rutina_a", Arrays.asList("A1","A2")), "persona_1", "persona_1");
-        Machine machine = new Machine("A1","maquina_1");
-        FrontController frontController = new FrontController();
-        boolean validar = frontController.validar(person, machine);
-        System.out.println("Validar: " + validar);
+    private InitCore initCore;
 
-    }*/
+    public HomeController(){
+        this.initCore = new InitCore();
+    }
 
     public boolean validar(Person persona, Machine machine){
-        RoutineValidator routineValidator = new RoutineValidator();
-        boolean validate = routineValidator.validate(persona, machine);
-        System.out.println("Validar: " + validate);
-        return validate;
+        return  initCore.executeValidators(persona,machine);
     }
 
 }
