f36425590237ecfd5e4cf5e7f7a15e4fe9b9dd02
jferdev <fernando.ene@besysoft.com>
2023-05-07 00:03:31 -0300

    changes

--- src/main/java/frontControllers/HomeController.java
+++ src/main/java/frontControllers/HomeController.java
@@ -4,7 +4,6 @@ import interfaces.Observer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import interfaces.Observer;
 import services.ValidationEngine;
 import views.Home;
 
