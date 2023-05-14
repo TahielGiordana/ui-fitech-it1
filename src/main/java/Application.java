2e2880b08490d248278a930aa3076e5961f74dca
Tony-L-93 <tonyliendro@gmail.com>
2023-05-13 21:18:58 -0300

    Se cambia para que se pase por args el codigo de maquina y el path

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,21 @@
 import core.CoreFitech;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import views.Home;
 
 import java.io.File;
 
 public class Application {
+    private static final Logger log = LogManager.getLogger("Application");
 
     public static void main(String[] args) {
-       CoreFitech coreFitech = new CoreFitech(new File("").getAbsolutePath());
-       Home home = new Home(coreFitech);
+        if(args.length < 2){
+            log.info("Debe proporcionar el path al directorio y el código de la máquina");
+            System.exit(0);
+        }
+        String path = args[0];
+        String machineCode = args[1];
+        CoreFitech coreFitech = new CoreFitech(path, machineCode);
+        Home home = new Home(coreFitech);
     }
 }
